20a7a48f02ff075e6a3188966a58b5b25c62dfc9
Sergio <sergioscardigno82@gmail.com>
2025-10-21 11:52:16 -0300

    popup delete

--- src/app/components/ExitIntentProvider.tsx
+++ src/app/components/ExitIntentProvider.tsx
@@ -1,30 +1,13 @@
 "use client";
 
 import React from "react";
-import { useExitIntent } from "@/hooks/useExitIntent";
-import ExitIntentPopup from "./ExitIntentPopup";
-import ScrollTriggeredPopup from "./ScrollTriggeredPopup";
 
 interface ExitIntentProviderProps {
   children: React.ReactNode;
 }
 
 const ExitIntentProvider: React.FC<ExitIntentProviderProps> = ({ children }) => {
-  const { showExitIntent, closeExitIntent } = useExitIntent();
-
-  return (
-    <>
-      {children}
-      <ExitIntentPopup 
-        isVisible={showExitIntent} 
-        onClose={closeExitIntent} 
-      />
-      <ScrollTriggeredPopup 
-        threshold={40}
-        delay={3000}
-      />
-    </>
-  );
+  return <>{children}</>;
 };
 
 export default ExitIntentProvider;
